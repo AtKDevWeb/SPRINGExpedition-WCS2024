b3345d1f98f4a7807372c9e5e17870c330eef212
KDevWeb <99359640+AtKDevWeb@users.noreply.github.com>
2025-04-09 06:56:50 +0200

    Ajout des Class manquantes pour le CRUD

--- myBlog/src/main/java/org/wcs/myBlog/mappers/ArticleMapper.java
+++ myBlog/src/main/java/org/wcs/myBlog/mappers/ArticleMapper.java
@@ -40,4 +40,35 @@ public class ArticleMapper {
         }
         return articleDTO;
     }
+
+//    public ArticleDTO convertToEntity(Article article) {
+//        ArticleCreateDTO articleDTO = new ArticleDTO();
+//
+//        articleDTO.setId(article.getId());
+//        articleDTO.setTitle(article.getTitle());
+//        articleDTO.setContent(article.getContent());
+//        articleDTO.setUpdateAt(article.getUpdatedAt());
+//
+//        if (article.getCategory() != null) {
+//            articleDTO.setCategoryName((article.getCategory().getName()));
+//        }
+//        if (article.getImages() != null) {
+//            articleDTO.setImagePaths((article.getImages().stream().map(Image::getPath).collect(Collectors.toList())));
+//        }
+//        if (article.getArticleAuthors()!=null) {
+//            articleDTO.setAuthors( article.getArticleAuthors().stream()
+//                    .filter(articleAuthor -> articleAuthor.getAuthor() != null)
+//                    .map(articleAuthor -> {
+//                        AuthorDTO authorDTO = new AuthorDTO();
+//                        authorDTO.setId(articleAuthor.getAuthor().getId());
+//                        authorDTO.setFirstName(articleAuthor.getAuthor().getFirstName());
+//                        authorDTO.setLastName(articleAuthor.getAuthor().getLastName());
+//
+//                        return authorDTO;
+//                    })
+//                    .collect(Collectors.toList()));
+//        }
+//        return articleDTO;
+//    }
+
 }
